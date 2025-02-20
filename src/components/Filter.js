07ead661ea37e2418fffb56b31bc950e7e40bc73
Faith Kimani <wanjikukimani24@gmail.com>
2025-02-20 04:11:02 +0000

    Feat: add search functionality

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-function Filter({ onCategoryChange }) {
+function Filter({ onCategoryChange, search, onSearchChange, selectedCategory }) {
   return (
     <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
-      <select name="filter" onChange={onCategoryChange}>
+      <input type="text" value={search} name="search" placeholder="Search..." onChange={onSearchChange} />
+      <select name="filter" value={selectedCategory} onChange={onCategoryChange}>
         <option value="All">Filter by category</option>
         <option value="Produce">Produce</option>
         <option value="Dairy">Dairy</option>
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,21 +5,33 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [search, setSearch] = useState("")
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
+  function handleSearchChange(e){
+    setSearch(e.target.value)
+
+  }
+  console.log(items)
+
   const itemsToDisplay = items.filter((item) => {
     if (selectedCategory === "All") return true;
 
     return item.category === selectedCategory;
-  });
+  }).filter(item=> item.name.toLowerCase().includes(search.toLowerCase()));
 
   return (
     <div className="ShoppingList">
       <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <Filter 
+      onCategoryChange={handleCategoryChange} 
+      search={search} 
+      onSearchChange={handleSearchChange}
+      selectedCategory={selectedCategory}
+      />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
